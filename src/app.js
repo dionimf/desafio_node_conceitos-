31b682b5ab0d0ea584b93b3f1fd8be123aa23552
dionimf <dionimf@unesc.net>
2020-10-26 23:19:26 -0300

    adicionado rotas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+ const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,11 +11,22 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body;
+  const repository ={
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+  
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
@@ -27,7 +38,16 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repository = repositories.find(repository => repository.id ===id);
+
+  if(!repository){
+    return response.status(400).send();
+  }
+
+  repository.likes ++;
+  return response.json(repository);
+
 });
 
 module.exports = app;
